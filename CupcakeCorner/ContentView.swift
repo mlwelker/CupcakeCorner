d52f3572e96a5e95d2394a938f3a39faed998670
mlwelker <nutria.watts.0g@icloud.com>
2023-08-25 11:49:14 -0500

    basic order details

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -2,28 +2,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
+    @StateObject var order = Order()
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create Account") {
-                    print("Creating account...")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Special requests", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery Details")
+                    }
                 }
             }
-//            .disabled(username.isEmpty || email.isEmpty)
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
         }
-    }
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
     }
 }
 
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,21 @@
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Chocolate", "Strawberry", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}

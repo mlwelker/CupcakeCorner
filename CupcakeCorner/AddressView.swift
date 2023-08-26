a9ad4ad922dc974c6298b22f99f7c8e3f0191461
mlwelker <nutria.watts.0g@icloud.com>
2023-08-26 13:17:36 -0500

    project completed + challenge 1 & 2

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,36 @@
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Checkout")
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddressView(order: Order())            
+        }
+    }
+}
